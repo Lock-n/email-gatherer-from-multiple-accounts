8b727fdc472fd978888a29482bc3cf6caa724916
Erik Kenzo Valle <50238171+ErikKenzoValle@users.noreply.github.com>
2019-06-11 17:35:19 -0300

    Update create_tables.sql
    added drop table and select too

--- off-project-files/create_tables.sql
+++ off-project-files/create_tables.sql
@@ -1,23 +1,31 @@
-create table TDI_project_Email_Account(
-	 email	varchar(200),
-     password		varchar(50) not null,
-     server_address	varchar(150) not null,
-     server_protocol	varchar(4) not null,
-     server_port	 int not null,
-     constraint pk_TDI_project_Email_Account primary key (email, server_address)
-)
+drop table TDI_project_Email_Account
+drop table TDI_project_User
 
+select * from TDI_project_Email_Account
+select * from TDI_project_User
 
 create table TDI_project_User(
-name varchar(100) primary key,
-password varchar(50) not null
+	name varchar(100)    primary key,
+	password varchar(50) not null
 )
 
+create table TDI_project_Email_Account(
+     email			varchar(200),
+     password			varchar(50) not null,
+     server_address		varchar(150) not null,
+     server_send_protocol	varchar(4) not null,
+     server_receive_protocol	varchar(4) not null,
+     server_send_port 		int not null,
+     server_receive_port	int not null,
+     name_user 			varchar(100) not null,
+     primary key (email, server_address),
+     constraint fk_TDI_project_User foreign key (name_user) references TDI_project_User(name)
+)
 
 create table TDI_project_User_Email_Accounts(
-email varchar(200) not null,
-name_user    varchar(100) not null,
-server_address varchar(150) not null,
+email 		varchar(200) not null,
+name_user       varchar(100) not null,
+server_address  varchar(150) not null,
 constraint fk_TDI_project_Email_Account foreign key (email, server_address) references TDI_project_Email_Account (email, server_address),
 constraint fk_TDI_project_User foreign key (name_user) references TDI_project_User (name),
 constraint pk_TDI_project_User_Email_Accounts primary key (email, name_user)
