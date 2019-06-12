103da066f6288aee6907a82d19e689a2a4c74429
Erik Valle <30930748+ErikKenzoValle@users.noreply.github.com>
2019-06-12 10:24:16 -0300

    Update EmailAccounts.java and EmailAccount.java
    Added the new column values:
    server_send_address
    server_receive_address

--- src/bd/dbos/EmailAccount.java
+++ src/bd/dbos/EmailAccount.java
@@ -5,7 +5,8 @@ public class EmailAccount implements Cloneable
 	
     private String email;
     private String password;
-    private String server_address;  // endereco do servidor de env
+    private String server_send_address;  // endereco do servidor de env
+    private String server_receive_address;  // endereco do servidor de recebimento
     private String server_send_protocol;	// protocolo de envio 
     private String server_receive_protocol;	// protocolo de recebimento
     private int    server_send_port;		// porta de envio
@@ -28,45 +29,52 @@ public class EmailAccount implements Cloneable
 		this.password = password;
 	}
 
-	public void setServer_address(String server_address)throws Exception
+	public void setServer_send_address(String server_send_address)throws Exception
 	{
 		if (server_address==null || server_address.equals(""))
-            throw new Exception ("endereco do servidor de envio não fornecido");
+            throw new Exception ("Endereco do servidor de envio não fornecido");
 		this.server_address = server_address;
 	}
 
-	public void setServer_send_protocol(String server_protocol)throws Exception
+	public void setServer_receive_address(String server_receive_address)throws Exception
 	{
-		if (server_protocol==null || server_protocol.equals(""))
-            throw new Exception ("protocolo de envio não fornecido");
-		this.server_send_protocol = server_protocol;
+		if (server_receive_address==null || server_receive_address.equals(""))
+            throw new Exception ("Endereco do servidor de recebimento não fornecido");
+		this.server_receive_address = server_receive_address;
 	}
 
-	public void setServer_receive_protocol(String server_protocol)throws Exception
+	public void setServer_send_protocol(String server_send_protocol)throws Exception
 	{
-		if (server_protocol==null || server_protocol.equals(""))
-            throw new Exception ("protocolo de envio não fornecido");
-		this.server_receive_protocol = server_protocol;
+		if (server_send_protocol==null || server_send_protocol.equals(""))
+            throw new Exception ("Protocolo de envio não fornecido");
+		this.server_send_protocol = server_send_protocol;
 	}
 
-	public void setServer_send_port(int server_port)throws Exception
+	public void setServer_receive_protocol(String server_receive_protocol)throws Exception
 	{
-		if (server_port <= 0)
+		if (server_receive_protocol==null || server_receive_protocol.equals(""))
+            throw new Exception ("Protocolo de recebimento não fornecido");
+		this.server_receive_protocol = server_receive_protocol;
+	}
+
+	public void setServer_send_port(int server_send_port)throws Exception
+	{
+		if (server_send_port <= 0)
             throw new Exception ("Porta de envio invalida");
-		this.server_send_port = server_port;
+		this.server_send_port = server_send_port;
 	}
 
-	public void setServer_receive_port(int server_port)throws Exception
+	public void setServer_receive_port(int server_receive_port)throws Exception
 	{
-		if (server_port <= 0)
-            throw new Exception ("Porta de envio invalida");
-		this.server_receive_port = server_port;
+		if (server_receive_port <= 0)
+            throw new Exception ("Porta de recebimento invalida");
+		this.server_receive_port = server_receive_port;
 	}
     
     public void setNameUser(String name_user)throws Exception
 	{
 		if (name_user==null || name_user.equals(""))
-            throw new Exception ("nome de usuario não fornecido");
+            throw new Exception ("Nome de usuario não fornecido");
 		this.name_user = name_user;
 	}
 
@@ -78,8 +86,12 @@ public class EmailAccount implements Cloneable
 		return this.password;
 	}
 
-	public String getServer_address() {
-		return this.server_address;
+	public String getServer_send_address() {
+		return this.server_send_address;
+	}
+
+	public String getServer_receive_address() {
+		return this.server_receive_address;
 	}
 
 	public String getServer_send_protocol() {
@@ -104,7 +116,7 @@ public class EmailAccount implements Cloneable
 
 
     
-    public EmailAccount(String email, String password, String server_address, String server_send_protocol, String server_receive_protocol, int server_send_port, int server_receive_port, String name_user)
+    public EmailAccount(String email, String password, String server_send_address, String server_receive_address, String server_send_protocol, String server_receive_protocol, int server_send_port, int server_receive_port, String name_user)
     {
 		this.email                = email;
 		this.password             = password;
@@ -121,7 +133,8 @@ public class EmailAccount implements Cloneable
 		String  ret = ""; 
 				ret += "email=" + email;
 				ret += "\n password=" + password;
-				ret+=  "\n server_address=" + server_address;
+				ret+=  "\n server_send_address=" + server_send_address;
+				ret+=  "\n server_receive_address=" + server_receive_address;
 				ret+=  "\n server_send_protocol=" + server_send_protocol;
 				ret+=  "\n server_receive_protocol=" + server_receive_protocol;
 				ret+=  "\n server_send_port=" + server_send_port;
@@ -137,7 +150,8 @@ public class EmailAccount implements Cloneable
 
 		ret = 7 * ret + email.hashCode();
 		ret = 7 * ret + password.hashCode();
-		ret = 7 * ret + server_address.hashCode();
+		ret = 7 * ret + server_send_address.hashCode();
+		ret = 7 * ret + server_receive_address.hashCode();
 		ret = 7 * ret + server_send_protocol.hashCode();
 		ret = 7 * ret + server_receive_protocol.hashCode();
 		ret = 7 * ret + new Integer(server_send_port).hashCode();
@@ -164,7 +178,10 @@ public class EmailAccount implements Cloneable
 		if (!this.password.equals(conEmail.password))
 			return false;
 		 
-		if (this.server_address != conEmail.server_address)
+		if (this.server_send_address != conEmail.server_send_address)
+			return false;
+		 
+		if (this.server_receive_address != conEmail.server_receive_address)
 			return false;
 		
 		if (!this.server_send_protocol.equals(conEmail.server_send_protocol))
@@ -191,7 +208,8 @@ public class EmailAccount implements Cloneable
     {
     	this.email      = modelo.email;      	// nao clono, pq nao eh clonavel
     	this.password      = modelo.password;			// nao clono, pq nao eh clonavel
-    	this.server_address = modelo.server_address;	// nao clono, pq nao eh clonavel
+    	this.server_send_address = modelo.server_send_address;	// nao clono, pq nao eh clonavel
+    	this.server_receive_address = modelo.server_receive_address;	// nao clono, pq nao eh clonavel
     	this.server_send_protocol    = modelo.server_send_protocol; 		// nao clono, pq nao eh clonavel
     	this.server_receive_protocol    = modelo.server_receive_protocol; 		// nao clono, pq nao eh clonavel
     	this.server_send_port    = modelo.server_send_port;		// nao clono, pq nao eh objeto
